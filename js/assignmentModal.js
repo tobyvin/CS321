085b76f0e54d89e748750316a05edbbeda4a1ad4
Toby Vincent <tobyv13@gmail.com>
2021-04-20 23:23:33 -0500

    got modal working

---
+++ js/assignmentModal.js
@@ -0,0 +1,23 @@
+// var assignmentModal = document.getElementById('assignmentModal')
+// assignmentModal.addEventListener('show.bs.modal', function (event) {
+//     // Button that triggered the modal
+//     var button = event.relatedTarget
+//     // Extract info from data-bs-* attributes
+//     var recipient = button.getAttribute('data-bs-whatever')
+//     // If necessary, you could initiate an AJAX request here
+//     // and then do the updating in a callback.
+//     //
+//     // Update the modal's content.
+//     var modalTitle = assignmentModal.querySelector('.modal-title')
+//     var modalBodyInput = assignmentModal.querySelector('.modal-body input')
+
+//     modalTitle.textContent = 'New message to ' + recipient
+//     modalBodyInput.value = recipient
+// })
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.mdb.modal', () => {
+  myInput.focus()
+})
--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 function login() {
     var username = document.getElementById("username").value;
-    if (username == "mbarson@siue.edu")  // parent login
+    if (username == "mbarson@school.edu")  // parent login
         window.location = "studentSelect.html";
-    else if (username == "fbarson@siue.edu") // student login
+    else if (username == "fbarson@school.edu") // student login
         window.location = "overview.html";
     else
         return
